2be0e64ac4199710be278961c0605c3331078e4f
Eric Sims <58200144+EricPostMaster@users.noreply.github.com>
2024-12-31 15:36:20 -0700

    Change loader for production deployment!

--- timelines/vite.config.js
+++ timelines/vite.config.js
@@ -2,15 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    loader: {
-      '.js': 'jsx', // Enable JSX in .js files
-    },
+    loader: 'jsx', // Set the loader to 'jsx' for all files
   },
 });
